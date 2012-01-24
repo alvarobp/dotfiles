d24d1f0a3bbffb71c1e610a74a6d15bfd07b718e
Alvaro Bautista <alvarobp@gmail.com>
2012-01-24 23:55:29 +0100

    Wasn't it obvious
    that we wanted to be able to overwrite predefined
    aliases by loading $HOME/.aliases ?

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -1,9 +1,9 @@
-[[ -s $HOME/.aliases ]] && source $HOME/.aliases
-
 alias rssh='rsync -avvurP -e ssh --delete '
 alias wgetn='wget --no-check-certificate '
 alias be='bundle exec '
 alias ber='bundle exec rspec'
+
+alias emacs='emacs -nw'
 
 if [ -d /Applications/Emacs.app ]; then
   alias eed='open -a /Applications/Emacs.app "$@"'
@@ -13,3 +13,5 @@ else
   alias eed='emacs'
   alias need='eed'
 fi
+
+[[ -s $HOME/.aliases ]] && source $HOME/.aliases
