8822df62c449f20e2290d94d0e67b8fc0d544474
Alvaro Bautista <alvarobp@gmail.com>
2012-05-28 16:22:21 +0200

    Do not alias emacs

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -3,8 +3,6 @@ alias wgetn='wget --no-check-certificate '
 alias be='bundle exec '
 alias ber='bundle exec rspec'
 
-alias emacs='emacs -nw'
-
 if [ -d /Applications/Emacs.app ]; then
   alias eed='open -a /Applications/Emacs.app "$@"'
   alias need='open -a /Applications/Emacs.app -n '
