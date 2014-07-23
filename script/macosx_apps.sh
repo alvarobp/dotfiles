bcb8018a9a7a5dc618392ecbe961e68d2a3805c0
Álvaro Bautista Pino <alvarobp@gmail.com>
2014-07-23 10:24:11 +0200

    Add installation of Xcode command line tools and homebrew to macosx apps script

--- script/macosx_apps.sh
+++ script/macosx_apps.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+xcode-select --install
+
+echo "Go and install Xcode Command Line Tools."
+echo "Press any key to continue"
+read cont
+
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
 if ! command -v brew >/dev/null 2>&1; then
   echo "You need to install Homebrew first. (http://brew.sh)"
   exit 1
