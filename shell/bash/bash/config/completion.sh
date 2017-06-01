0fb2b6f0903cbd28a64efdf9129d5673d343f6c0
Alvaro Bautista <alvarobp@gmail.com>
2017-06-01 18:47:56 +0200

    Add git completion in bash

--- shell/bash/bash/config/completion.sh
+++ shell/bash/bash/config/completion.sh
@@ -1,11 +1,1 @@
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
-if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
-    . /etc/bash_completion
-fi
-
-if in_osx && which brew &> /dev/null; then
-  path="$(brew --prefix)/etc/bash_completion"
-  [[ -f $path ]] && source $path
-fi
+for config_file in `ls $HOME/.bash/config/completion/*.sh`; do source $config_file; done
