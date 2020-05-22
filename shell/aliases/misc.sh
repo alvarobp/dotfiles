7e1c95c8ddb60e46d6bc055a71ca4c8b6d8f0e66
Alvaro Bautista <alvarobp@gmail.com>
2020-05-22 08:10:45 +0200

    Add rshell alias to reload shell

--- shell/aliases/misc.sh
+++ shell/aliases/misc.sh
@@ -4,5 +4,6 @@ alias ccd='cd $HOME/code'
 alias curlo='curl -O'
 alias eed='emacs'
 alias rssh='rsync -avvurP -e ssh --delete '
+alias rshell='exec $SHELL' # reload shell
 alias trc='truncate -s0'
 alias wgetn='wget --no-check-certificate '
