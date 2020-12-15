2acbf6203b7e7f426700de7d3fea65096f1ed8aa
Alvaro Bautista <alvarobp@gmail.com>
2020-12-15 07:33:20 +0100

    Add pcopy alias

--- shell/aliases/misc.sh
+++ shell/aliases/misc.sh
@@ -3,6 +3,7 @@ alias ack='ack -Q'
 alias ccd='cd $HOME/code'
 alias curlo='curl -O'
 alias eed='emacs'
+which xclip &> /dev/null && alias pcopy='xclip -selection clipboard'
 alias rssh='rsync -avvurP -e ssh --delete '
 alias rshell='exec $SHELL' # reload shell
 alias trc='truncate -s0'
