eb27984b8709a6dd350991bc1621183807f1682f
Alvaro Bautista <alvarobp@gmail.com>
2024-06-12 19:39:39 +0200

    Configure neotree plugin in neovim

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -23,15 +23,40 @@ local plugins = {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {"nvim-treesitter/nvim-treesitter", build= ":TSUpdate"},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    }
+
   }
 }
 
 require("lazy").setup(plugins, opts)
 
+-- Catpuccin colorscheme
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
 
+-- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+-- Treesitter
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "javascript"},
+  highlight = { enable = true },
+  indent = { enable = true }
+})
+
+-- Neotree
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>')
 
