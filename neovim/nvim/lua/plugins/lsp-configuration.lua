f6576c08e6453f2eb4dbc19220fda36c02c12ae1
Alvaro Bautista <alvarobp@gmail.com>
2024-06-12 19:43:42 +0200

    Configure lua LSP and telescope-ui-select

---
+++ neovim/nvim/lua/plugins/lsp-configuration.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", '<space>rn', vim.lsp.buf.rename, {})
+    end
+  }
+}
