8d78b79ca0736fa1b0fadf37db08dc6ffa2356f8
Alvaro Bautista <alvarobp@gmail.com>
2024-06-12 19:40:32 +0200

    Extract file for each plugin in neovim

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -17,44 +17,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {}
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build= ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    }
-
-  }
-}
-
-require("lazy").setup(plugins, opts)
-
--- Catpuccin colorscheme
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
--- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
--- Treesitter
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "javascript"},
-  highlight = { enable = true },
-  indent = { enable = true }
-})
+require("lazy").setup("plugins")
 
 -- Neotree
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
