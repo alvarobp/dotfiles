b8e33940eace94d283547a22735f6b7d867dbee7
Alvaro Bautista <alvarobp@gmail.com>
2024-06-12 19:33:25 +0200

    Configure Lazy package manager in neovim

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -3,3 +3,21 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader=" "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {}
+local plugins = {}
+
+require("lazy").setup(plugins, opts)
