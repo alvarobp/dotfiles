91ed251579d13904906874bc5c64e7465d2828bc
Alvaro Bautista <alvarobp@gmail.com>
2024-06-14 00:25:39 +0200

    Configure rubocop in neovim

--- neovim/nvim/lua/plugins/none-ls.lua
+++ neovim/nvim/lua/plugins/none-ls.lua
@@ -1,13 +1,15 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-			},
-		})
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.formatting.rubocop,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
