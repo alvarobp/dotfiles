ee1ef74566f48347db62c980859f0fdb77a38774
Alvaro Bautista <alvarobp@gmail.com>
2024-06-20 22:54:34 +0200

    Enable number option in neovim

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -4,6 +4,8 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader=" "
 
+vim.opt.number = true
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
